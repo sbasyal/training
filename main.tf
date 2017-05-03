f3df0f468ea0c51b52d16825c5d231ba9a429683
HashiCorp Training <training@hashicorp.com>
2017-05-03 23:16:44 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,65 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your AMI ID is:
+#
+#     ami-2df66d3b
+#
+# Your subnet ID is:
+#
+#     subnet-fe40a9a4
+#
+# Your security group ID is:
+#
+#     sg-4cc10432
+#
+# Your Identity is:
+#
+#     testing-elk
+#
+
+terraform {
+  backend "atlas" {
+    name = "sbasyal/train"
+  }
+}
+
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "num" {
+  default = "2"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "web" {
+  # ...
+  count = 2
+  ami                    = "ami-2df66d3b"
+  subnet_id              = "subnet-fe40a9a4"
+  vpc_security_group_ids = ["sg-4cc10432"]
+  instance_type          = "t2.micro"
+
+  tags {
+    "Identity" = "testing-elk"
+    "Name"     = "sabin-testing-elk-${count.index+1}/${var.num}"
+  }
+}
+
+  output "PublicIP" {
+    value = ["${aws_instance.web.*.public_ip}"]
+  }
+
+  output "PublicDNS" {
+    value = ["${aws_instance.web.*.public_dns}"]
+  }
